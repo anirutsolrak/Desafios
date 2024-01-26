92fe56ff14845145b275c360f1d2254bdd43e941
anirutsolrak <146773453+anirutsolrak@users.noreply.github.com>
2024-01-26 10:35:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,34 @@
-function escolherCaminho(opcao) {
-    let escolhaDoCaminho = document.getElementById("resultado");
-    let escolhaA = document.getElementById("opcao1");
-    let escolhaB = document.getElementById("opcao2");
-    let botaoProximo = document.getElementById("opcao3");
+let armas = [];
+let armaduras = [];
+let pocoes = [];
+let fim;
 
-    if (opcao === 1) {
-        escolhaDoCaminho.textContent = 'Você escolheu o caminho da esquerda. Algo aconteceu!';
-        escolhaA.disabled = true;
-        escolhaB.disabled = true;
-        botaoProximo.style.display = 'block'; // Torna o botão "Próximo" visível
-    } else if (opcao === 2) {
-        escolhaDoCaminho.textContent = 'Você escolheu o caminho da direita. Algo diferente aconteceu!';
-        escolhaA.disabled = true;
-        escolhaB.disabled = true;
-        botaoProximo.style.display = 'block'; // Torna o botão "Próximo" visível
+alert('Bem vindo ao organizador de inventário');
+
+while (fim !== "nao") {
+    let item = prompt('Qual item você deseja incluir no inventário');
+    alert('Em qual categoria você deseja por esse item?');
+    let categoria = prompt('armas? armaduras? pocoes?');
+
+    if (categoria === "armas") {
+        armas.push(item);
+    } else if (categoria === "armaduras") {
+        armaduras.push(item);
+    } else if (categoria === "pocoes") {
+        pocoes.push(item);
     }
-if (botaoProximo.style.display === 'block') {
-    escolhaA.disabled = false;
-    escolhaB.disabled = false;
+
+    alert('Deseja incluir mais algum item no inventário?');
+    fim = prompt('sim ou nao?');
 }
+
+alert('Gostaria de ver os itens armazenados?');
+let confirmacao = prompt('sim ou nao?');
+
+if (confirmacao === 'sim') {
+    alert('Armas: ' + armas.join());
+    alert('Armaduras: ' + armaduras.join());
+    alert('Poções: ' + pocoes.join());
+} else {
+    alert('Até breve');
 }
