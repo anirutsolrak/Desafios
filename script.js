497a8a73e5f3b887fa602bc08aa6ae66e4409c48
anirutsolrak <146773453+anirutsolrak@users.noreply.github.com>
2024-01-29 11:06:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
+let resultados =[];
 function (criptografar) {
    let a = ai;
     let e = enter;
     let i = imes;
     let o = ober;
     let u = ufat;
-    }
-let input = getElementeById ("input").value;
+    let input = getElementeById ("input").value;
     console.log (input)
+    //aqui verifica elemento por elemento no campo input
+    for (a=0; a<input.length; a++){
+        //aqui modifica a letra
+        let letra =input[a];
+        //aqui adiciona a letra modificada na lista
+        resultados.push (letra)
+        
+         //aqui verifica elemento por elemento no campo input
+    for (e=0; a<input.length; e++){
+        //aqui modifica a letra
+        let letra =input[e];
+        //aqui adiciona a letra modificada na lista
+        resultados.push (letra)
+        
+         //aqui verifica elemento por elemento no campo input
+    for (i=0; a<input.length; i++){
+        //aqui modifica a letra
+        let letra =input[i];
+        //aqui adiciona a letra modificada na lista
+        resultados.push (letra)
+        
+         //aqui verifica elemento por elemento no campo input
+    for (o=0; a<input.length; a++){
+        //aqui modifica a letra
+        let letra =input[o];
+        //aqui adiciona a letra modificada na lista
+        resultados.push (letra)
+        
+         //aqui verifica elemento por elemento no campo input
+    for (u=0; a<input.length; u++){
+        //aqui modifica a letra
+        let letra =input[u];
+        //aqui adiciona a letra modificada na lista
+        resultados.push (letra)
+    }
+        getElementById (processar).addEventListener("click", criptografar)
+        alert resultado
